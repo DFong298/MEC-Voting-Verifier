bf81eeeaaca2740d7975def6077b5f7c9d3e2d87
Willie Pai <PaisWillie@gmail.com>
2021-10-23 12:22:31 -0400

    Fixed SQL command

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ voter_list = pandas.read_excel('MEC_Voter_Data.xls')
 con = sqlite3.connect('Verify_Voters.db')
 cur = con.cursor()
 
-cur.execute("""CREATE TABLE valid_voters (first_name TINYTEXT, last_name TINYTEXT,party TINYTEXT,)""")
+cur.execute("CREATE TABLE valid_voters (first_name TINYTEXT, last_name TINYTEXT, party TINYTEXT)")
 
 valid_voter_list = []
 valid_parties = ["Socks and Crocs Reform League", "Pineapple Pizza Party", "Pronounced Jiff Union"]
